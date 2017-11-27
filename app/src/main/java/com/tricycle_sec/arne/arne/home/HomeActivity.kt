21f9ae386fdc6e6aecfa4c282e3f73769d8c80fd
Hans <hans3132@live.nl>
2017-11-27 13:43:10 +0100

    adding child check functions

--- app/src/main/java/com/tricycle_sec/arne/arne/home/HomeActivity.kt
+++ app/src/main/java/com/tricycle_sec/arne/arne/home/HomeActivity.kt
@@ -5,6 +5,7 @@ import com.tricycle_sec.arne.arne.R
 import com.tricycle_sec.arne.arne.base.BaseActivity
 import com.google.firebase.auth.FirebaseAuth
 import android.content.Intent
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.database.DataSnapshot
@@ -14,6 +15,10 @@ import com.tricycle_sec.arne.arne.firebase.CurrentStatus
 import com.tricycle_sec.arne.arne.firebase.Example
 import com.tricycle_sec.arne.arne.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_home.*
+import com.google.firebase.database.ChildEventListener
+import com.tricycle_sec.arne.arne.firebase.UserGeneralInfo
+import java.sql.SQLOutput
+
 
 class HomeActivity : BaseActivity() {
 
@@ -23,6 +28,8 @@ class HomeActivity : BaseActivity() {
 
         getExampleData()
         getTestData()
+        getCurrentStatuss()
+        getUsersGeneralInfo()
     }
 
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
@@ -67,17 +74,58 @@ class HomeActivity : BaseActivity() {
         getDatabaseReference(TEST_PATH).addValueEventListener(eventListener)
     }
 
-    fun getCurrentStatus() {
-        val eventListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                test_text.text =  dataSnapshot.getValue(CurrentStatus::class.java).uuid
+    fun getUsersGeneralInfo() {
+        val ref = getDatabaseReference(USER_PATH)
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, prevChildKey: String?) {
+                val addedUser = dataSnapshot.getValue<UserGeneralInfo>(UserGeneralInfo::class.java)
+                if (addedUser != null) {
+                    println("addeduser: " + addedUser.fname)
+                    println("addeduser: " + addedUser.lname)
+                }
             }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-
+            override fun onChildChanged(dataSnapshot: DataSnapshot, prevChildKey: String?) {
+                val changeduser = dataSnapshot.getValue<UserGeneralInfo>(UserGeneralInfo::class.java)
+                if (changeduser != null) {
+                    println("changeduser: " + changeduser.fname)
+                    println("changeduser: " + changeduser.lname)
+                }
             }
-        }
-        getDatabaseReference(STATUS_PATH).addValueEventListener(eventListener)
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, prevChildKey: String) {}
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
+    fun getCurrentStatuss() {
+        val ref = getDatabaseReference(STATUS_PATH)
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, prevChildKey: String?) {
+                val addedUser = dataSnapshot.getValue<CurrentStatus>(CurrentStatus::class.java)
+                if (addedUser != null) {
+                    println("addeduser: " + addedUser.uuid)
+                    println("addeduser: " + addedUser.onLocation)
+                }
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, prevChildKey: String?) {
+                val changedUser = dataSnapshot.getValue<CurrentStatus>(CurrentStatus::class.java)
+                if (changedUser != null) {
+                    println("userchanged: " + changedUser.uuid)
+                    println("userchanged: " + changedUser.onLocation)
+                }
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, prevChildKey: String) {}
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
     }
 }
 
